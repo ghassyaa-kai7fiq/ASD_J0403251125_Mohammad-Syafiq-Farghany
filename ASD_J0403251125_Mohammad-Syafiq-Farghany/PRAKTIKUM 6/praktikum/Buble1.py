db0274fcc76e5555bc2241197e29acf58ddbca99
Muzavenio Syafiq Kairan <vashfarcybefiq@gmail.com>
2026-03-06 15:51:59 +0700

    Sorting & Searching (Pertemuan 6)
    Pembelajaran mengenai mengolah sebuah data yang besar jika ingin dapat dibaca maka harus diurutkan menggunakan sort program. 
    
    Note: masih ada beberapa yang perlu di revisi dan ditambahkan sebbagai pembelajaran ditunggu ya update-an-nya

---
+++ ASD_J0403251125_Mohammad-Syafiq-Farghany/PRAKTIKUM 6/praktikum/Buble1.py
@@ -0,0 +1,28 @@
+#=====================================================
+# Nama    : Mohammad Syafiq Farghany
+# NIM     : J0403251125
+# Kelas  : Teknologi Rekayasa Perangkat Lunak A1/P1
+#=====================================================
+
+#===================================================================================
+# Praktikum 6 : Buble sort Ascending
+# Latihan Dasar : Membandingkan dan Menukar Nilai besar & kecil Secara (Bertahap)
+#===================================================================================
+
+def bubbleSort(alist):
+    exchanges = True
+
+    passnum = len(alist)-1
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                exchanges = True
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+        passnum = passnum-1
+
+data = [54,26,93,17,77,31,44,55,20]
+bubbleSort(data)
+print(data)
